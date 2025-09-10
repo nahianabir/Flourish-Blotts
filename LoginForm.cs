4185a52143b6178b9f436dc1e4866c6eb930e5cb
nahianabir <mdprithom6@gmail.com>
2025-09-10 22:40:27 +0600

    login to admin and data connection #abir

--- LoginForm.cs
+++ LoginForm.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Flourish___Blotts
 {
     public partial class LoginForm : Form
     {
-
+        private DataAccess Da { get; set; }
         private string text;
         private int len = 0;
         private bool isFirstAnimationComplete = false;
@@ -21,6 +23,7 @@ namespace Flourish___Blotts
         public LoginForm()
         {
             InitializeComponent();
+            this.Da = new DataAccess();
         }
 
         
@@ -93,7 +96,44 @@ namespace Flourish___Blotts
             }
         }
 
-        
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userID = txtID.Text;
+            string password = txtPassword.Text;
 
+
+            if (string.IsNullOrWhiteSpace(userID) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter both Id and Password.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                // ❗ Using your DataAccess.ExecuteQueryTable
+                string sql = "SELECT * FROM Admin WHERE ID = '" + userID +
+                             "' AND Password COLLATE SQL_Latin1_General_CP1_CS_AS = '" + password + "';";
+
+                var dt = this.Da.ExecuteQueryTable(sql);
+
+                if (dt.Rows.Count == 1)
+                {
+                    MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // 👉 Open AdminPanelForm
+                    AdminPanelForm adminPanel = new AdminPanelForm();
+                    adminPanel.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid ID or Password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
     }
 }
